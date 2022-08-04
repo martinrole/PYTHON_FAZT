da176b0f2add16c565662e0287438874bdce1552
Martin Leon <martinrole@hotmail.com>
2022-08-04 18:41:18 -0500

    input.py updated

--- 05-inputs.py
+++ 05-inputs.py
@@ -1,8 +1,11 @@
 edad = input('Igresa edad: ')
+altura = float( input('Igresa edad: ') )
 
 ##Todo ingresa como si fuera un string:
 print( type(edad) )
+print( type(altura) )
 
 ##Tengo que convertir en un número para hacer operaciones matemáticas:
+print('---------------------------------------')
 nuevaEdad = int(edad) + 5
 print( nuevaEdad )
